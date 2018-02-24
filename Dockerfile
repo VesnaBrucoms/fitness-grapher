f083dc2372918acb6a0b067cb9ff32b3c219af18
Trevalyan <etstevens60@gmail.com>
2018-02-24 14:18:54 +0000

    Authentication and label retreival

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.6
 
 COPY ./ /tmp/fitness-grapher/
+COPY ./credentials.dat /tmp/fitness-grapher/credentials.dat
 WORKDIR /tmp/fitness-grapher/
 RUN python setup.py install
--- fitness_grapher/__main__.py
+++ fitness_grapher/__main__.py
@@ -1,4 +1,5 @@
 import httplib2
+import logging
 
 import click
 from apiclient.discovery import build
@@ -7,37 +8,48 @@ from oauth2client.file import Storage
 from oauth2client.client import AccessTokenRefreshError
 from oauth2client.client import OAuth2WebServerFlow
 
-# client_id = ''
-# client_secret = ''
+from . import service_details
 
-# scope = 'https://www.googleapis.com/auth/gmail.readonly'
 
-# flow = OAuth2WebServerFlow(client_id, client_secret, scope)
-
-# storage = Storage('credentials.dat')
-# credentials = storage.get()
-
-# if credentials is None or credentials.invalid:
-#     credentials = tools.run_flow(flow, storage, tools.argparser.parse_args())
-
-# http = httplib2.Http()
-# http = credentials.authorize(http)
-
-# service = build('gmail', 'v1', http=http)
-
-# try:
-#     response = service.users().labels().list(userId='me').execute()
-#     print(response)
-# except Exception as err:
-#     print(err)
+SCOPE = 'https://www.googleapis.com/auth/gmail.readonly'
 
 
 @click.command()
 @click.argument('client_id')
 @click.argument('client_secret')
-def main(client_id, client_secret):
-    print(client_id)
-    print(client_secret)
+@click.option('-l', '--log-level', default='INFO', help='Logging detail level')
+def main(client_id, client_secret, log_level):
+    logging.basicConfig(level=log_level)
+    logging.info('Fitness Grapher v%s\n', service_details['version'])
+
+    logging.info('Authenticating')
+    flow = OAuth2WebServerFlow(client_id, client_secret, SCOPE)
+
+    storage = Storage('credentials.dat')
+    credentials = storage.get()
+
+    if credentials is None or credentials.invalid:
+        flags = tools.argparser.parse_args('--auth_host_name localhost '
+                                           '--noauth_local_webserver'.split())
+        credentials = tools.run_flow(flow, storage, flags)
+
+    http = httplib2.Http()
+    http = credentials.authorize(http)
+    logging.info('Authentication successful')
+
+    logging.info('Building service')
+    service = build('gmail', 'v1', http=http)
+
+    try:
+        response = service.users().labels().list(userId='me').execute()
+
+        fitbit_label = None
+        for label in response['labels']:
+            if label['name'] == 'Fitbit':
+                fitbit_label = label
+                print(label)
+    except Exception as err:
+        print(err)
 
 
 if __name__ == '__main__':
